6e4747c0ce52cc1f046f2d3c63e9697b8d9d2537
Michael Lodder <redmike7@gmail.com>
2020-05-12 12:18:02 -0600

    Updates to build to include wee_alloc as optional
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-//TODO do we want this as our allocator?
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 pub mod bls12381;
 pub mod bbs_plus;
