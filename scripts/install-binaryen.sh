6aec11d2bd9a13f249432016bd9a258b73a38c2c
Tobias <tplooker@gmail.com>
2020-05-19 11:54:02 +1200

    feat: updates to install scripts

--- scripts/install-binaryen.sh
+++ scripts/install-binaryen.sh
@@ -1,17 +1,52 @@
+#!/usr/bin/env bash
+
 set -e
 
-echo "*** Downloading binaryen"
+echo ">>> Deleting old version of binaryen"
+
+rm -rf binaryen
+
+echo ">>> Creating output directory"
+
+mkdir -p binaryen/bin
+
+echo ">>> Downloading binaryen"
 
 # wasm2js for wasm -> asm.js
 BINARYEN=( "wasm-opt" "wasm2js" )
 
-mkdir -p binaryen/bin
 
-curl -L https://api.github.com/repos/WebAssembly/binaryen/releases/latest \
-    | grep "browser_download_url.*macos.tar.gz\"" \
-    | cut -d : -f 2,3 \
-    | tr -d '"' \
-    | wget -O binaryen-download.tar.gz -qi - \
-    | tar -xz
-#  
-# mkdir -p binaryen/bin
+unamestr=`uname`
+
+if [ "$unamestr" == 'Darwin' ]
+then
+    echo ">>>> Downloading MacOS version"
+    curl -L https://api.github.com/repos/WebAssembly/binaryen/releases/latest \
+        | grep "browser_download_url.*macos.tar.gz\"" \
+        | cut -d : -f 2,3 \
+        | tr -d '"' \
+        | wget -O binaryen-download.tar.gz -qi -
+elif [ "$unamestr" == 'Linux' ]
+then
+    echo ">>>> Downloading Linux version"
+    curl -L https://api.github.com/repos/WebAssembly/binaryen/releases/latest \
+        | grep "browser_download_url.*linux.tar.gz\"" \
+        | cut -d : -f 2,3 \
+        | tr -d '"' \
+        | wget -O binaryen-download.tar.gz -qi -
+else
+    echo ">>> Unable to identfy OS to download binaryen"
+    exit 1
+fi
+
+
+
+echo ">>> Extracting result"
+
+tar xvzf binaryen-download.tar.gz -C binaryen/bin
+
+rm binaryen-download.tar.gz
+
+cp -a `find ./binaryen/bin/* -maxdepth 1 -type d`/* binaryen/bin
+
+rm -rf `find ./binaryen/bin/* -maxdepth 1 -type d`
--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,5 +1,12 @@
 set -e
 
-echo "*** Installing Dependencies"
+echo ">>>> Installing Dependencies"
 
-./scripts/install-dependencies.sh
+
+if [ -d "binaryen/bin" ] 
+then
+    echo ">>>> Binaryen already installed, skipping" 
+else
+    echo ">>>> Binaryen not installed, starting install"
+    ./scripts/install-binaryen.sh
+fi
