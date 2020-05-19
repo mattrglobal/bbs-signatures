31d8859111c148ac3a56a6f6a29eb61771dba5f7
Tobias <tplooker@gmail.com>
2020-05-19 21:20:39 +1200

    build: add nightly toolchain

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,12 +6,15 @@ if [ -d "binaryen/bin" ]
 then
     echo ">>>> Binaryen already installed, skipping" 
 else
-    echo ">>>> Binaryen not installed, starting install"
+    echo ">>>> Binaryen not installed, installing binaryen"
     ./scripts/install-binaryen.sh
 fi
-
 
 if ! [ -x "$(command -v wasm-pack)" ]; then
     echo ">>> Installing wasm-pack"
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 fi
+
+echo ">>> Installing nightly toolchain"
+
+rustup toolchain install nightly
