f56537f3e8ba15654ccec78aa80d88cd0ab21107
Tobias <tplooker@gmail.com>
2020-05-19 21:29:13 +1200

    build: re-add optimize wasm-opt flags

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -12,7 +12,7 @@ OPT_WASM=lib/wasm_opt.wasm
 rustup run stable wasm-pack build --release --out-dir lib --target nodejs
 
 # Optimize WASM
-$WASM_OPT -o $OPT_WASM $WASM
+$WASM_OPT -O4 -Oz -o $OPT_WASM $WASM
 
 rm $WASM
 mv $OPT_WASM $WASM
