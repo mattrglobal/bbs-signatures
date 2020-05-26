86728bdab298af6ed8e4165b4db44202a54e8e0b
Tobias <tplooker@gmail.com>
2020-05-27 09:23:17 +1200

    build: change wasm-pack tests to use stable toolchain

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "*** Testing WASM on chrome"
-rustup run nightly wasm-pack test --headless --safari
+rustup run stable wasm-pack test --headless --chrome
 
 echo "*** Testing WASM with jest"
 yarn jest
