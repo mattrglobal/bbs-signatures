a272e50f2f7cee9676122507dc7fd68c6883fdb5
Tobias <tplooker@gmail.com>
2020-05-27 09:04:27 +1200

    fix: tweaks to api changes

--- src/js/index.d.ts
+++ src/js/index.d.ts
@@ -1,45 +1,12 @@
-export { bls12381ToBbs } from "./bls12381ToBbs"
-export { bls_generate_key as generateBls12381KeyPair} from "./wasm"
-
 export interface BlsKeyPair {
-    /**
-     * Raw public key value for the key pair
-     */
-    readonly publicKey: Uint8Array;
-    /**
-     * Raw secret/private key value for the key pair
-     */
-    readonly secretKey?: Uint8Array;
-}
-  
-/**
- * Converts a BLS12-381 key to a BBS+ public key
- */
-export interface Bls12381ToBbsRequest {
-    /**
-     * The BLS 12-381 key pair to convert
-     */
-    readonly keyPair: BlsKeyPair;
-    /**
-     * The number of messages for the BBS+ key
-     */
-    readonly messageCount: number;
+  /**
+   * Raw public key value for the key pair
+   */
+  readonly publicKey: Uint8Array;
+  /**
+   * Raw secret/private key value for the key pair
+   */
+  readonly secretKey?: Uint8Array;
 }
 
-/**
- * A BBS+ key pair
- */
-export interface BbsKeyPair {
-    /**
-     * Raw public key value for the key pair
-     */
-    readonly publicKey: Uint8Array;
-    /**
-     * Raw secret/private key value for the key pair
-     */
-    readonly secretKey?: Uint8Array;
-    /**
-     * Number of messages that can be sign
-     */
-    readonly messageCount: number;
-}
+export function generateBls12381KeyPair(seed?: Uint8Array): BlsKeyPair;
