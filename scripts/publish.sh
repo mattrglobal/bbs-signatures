0d1fb27a92318dc8e3745160585a6b8ae607bdfc
Tobias Looker <tobias.looker@mattr.global>
2020-06-04 22:20:12 +1200

    build: add support for removing post install script (#24)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,9 @@
 # Add dev dependencies to current path
 export PATH="$PATH:node_modules/.bin"
 
+# Remove the post install script from the package.json
+node scripts/remove-install-script.js
+
 # Fetch the current version from the package.json
 new_version=$(node -pe "require('./package.json').version")
 
