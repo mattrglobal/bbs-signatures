eddd7bc25a9f0122b3662bb3edea15f4bb132ee9
Haardik <hhaardik@uwaterloo.ca>
2020-06-18 06:00:54 -0400

    feat: (wip) generate asm and test it

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -5,6 +5,9 @@ set -e
 WASM_OPT=./binaryen/bin/wasm-opt
 WASM=lib/wasm_bg.wasm
 OPT_WASM=lib/wasm_opt.wasm
+WASM_2_JS=./binaryen/bin/wasm2js
+ASM=lib/wasm_asm.js
+SRC_WASM=lib/wasm.js
 
 # TODO deal with when rustup not installed?
 
@@ -15,9 +18,24 @@ rustup run stable wasm-pack build --release --out-dir lib --target nodejs
 $WASM_OPT -O4 -o $OPT_WASM $WASM
 
 rm $WASM
-mv $OPT_WASM $WASM
+cp $OPT_WASM $WASM
 
-# Delete the package.json, gitignore and readme automatically created by wasm-pack
+# Build asm.js version
+echo "Building asm.js version"
+$WASM_2_JS --output $ASM $WASM
+
+# cleanup generated output
+sed -i -e 's/import {/\/\/ import {/g' $ASM
+sed -i -e 's/function asmFunc/var bbs = require('\''\.\/wasm'\''); function asmFunc/g' $ASM
+sed -i -e 's/{abort.*},memasmFunc/bbs, memasmFunc/g' $ASM
+sed -i -e 's/export var /module\.exports\./g' $ASM
+
+# Polyfill TextDecoder as it causes issues in RN
+sed -i -e 's/const { TextDecoder } = require(String.raw`util`);/const { u8aToString } = require('\''\.\/util'\'');/g' $SRC_WASM
+sed -i -e 's/let cachedTextDecoder = new /\/\/ let cachedTextDecoder = new /g' $SRC_WASM
+sed -i -e 's/cachedTextDecoder\.decode/u8aToString/g' $SRC_WASM
+
+# Delete the gitignore and readme automatically created by wasm-pack
 rm lib/package.json lib/.gitignore lib/README.md
 
 # Copy over package sources
---
+++ src/js/util.js
@@ -0,0 +1,38 @@
+let decoder;
+
+function polyfilledDecode(value) {
+  return value.reduce((str, code) => {
+    return str + String.fromCharCode(code);
+  }, "");
+}
+
+try {
+  decoder = new TextDecoder("utf-8");
+} catch (error) {
+  decoder = {
+    decode: polyfilledDecode,
+  };
+}
+
+/**
+ * @name u8aToString
+ * @summary Creates a utf-8 string from a Uint8Array object.
+ * @description
+ * `UInt8Array` input values return the actual decoded utf-8 string. `null` or `undefined` values returns an empty string.
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * const { u8aToString } = require('./util');
+ *
+ * u8aToString(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0x6f])); // hello
+ * ```
+ */
+function u8aToString(value) {
+  if (!value || !value.length) {
+    return "";
+  }
+  return decoder.decode(value);
+}
+
+exports.u8aToString = u8aToString;
