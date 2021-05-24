bc3dabd97a155c1c96adb7aebb6629d922b5426e
Tobias Looker <tobias.looker@mattr.global>
2021-05-25 10:03:33 +1200

    test: improve createProof test coverage (#88)

--- tests/bbsSignature/createProof.bbsSignature.spec.ts
+++ tests/bbsSignature/createProof.bbsSignature.spec.ts
@@ -119,6 +119,114 @@ describe("bbsSignature", () => {
       expect(proof).toBeInstanceOf(Uint8Array);
       expect(proof.length).toEqual(415); //TODO evaluate this length properly add a reason for this and some constants?
     });
+
+    it("should fail to create proof when attempting to create one with an unsigned extra message", async () => {
+      const messages = [
+        stringToBytes("J42AxhciOVkE9w=="),
+        stringToBytes("PNMnARWIHP+s2g=="),
+        stringToBytes("ti9WYhhEej85jw=="),
+        stringToBytes("badmessagex01a=="),
+      ];
+
+      const bbsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pbiZ/pmArLDr3oSCqthKgSZw4VFzzJMFEuHP9AAnOnUJmqkOmvI1ctGLO6kCLFuwQVAAAAA4GrOHdyZEbTWRrTwIdz+KXWcEUHdIx41XSr/RK0TE5+qU7irAhQekOGFpGWQY4rYrDxoHToB4DblaJWUgkSZQLQ5sOfJg3qUJr9MpnDNJ8nNNitL65e6mqnpfsbbT3k94LBQI3/HijeRl29y5dGcLhOxldMtx2SvQg//kWOJ/Ug8e1aVo3V07XkR1Ltx76uzA=="
+      );
+      const signature = base64Decode(
+        "qg3PfohWGvbOCZWxcWIZ779aOuNSafjCXLdDux01TTNGm/Uqhr/kZZ1wSmxKwbEWAhctrDCp2mGE0M0l6DlA5R38chMbtnyWMfQgbQpzMQZgPBPUvVWivJyYEysZnQWrAYzZzRPe36VFbFy5ynWx0w=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: bbsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0, 1, 2, 3],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
+
+    it("should fail to create proof when attempting to create one with a modified message", async () => {
+      const messages = [
+        stringToBytes("badmessagex01a=="),
+        stringToBytes("PNMnARWIHP+s2g=="),
+        stringToBytes("ti9WYhhEej85jw=="),
+      ];
+
+      const bbsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pbiZ/pmArLDr3oSCqthKgSZw4VFzzJMFEuHP9AAnOnUJmqkOmvI1ctGLO6kCLFuwQVAAAAA4GrOHdyZEbTWRrTwIdz+KXWcEUHdIx41XSr/RK0TE5+qU7irAhQekOGFpGWQY4rYrDxoHToB4DblaJWUgkSZQLQ5sOfJg3qUJr9MpnDNJ8nNNitL65e6mqnpfsbbT3k94LBQI3/HijeRl29y5dGcLhOxldMtx2SvQg//kWOJ/Ug8e1aVo3V07XkR1Ltx76uzA=="
+      );
+      const signature = base64Decode(
+        "qg3PfohWGvbOCZWxcWIZ779aOuNSafjCXLdDux01TTNGm/Uqhr/kZZ1wSmxKwbEWAhctrDCp2mGE0M0l6DlA5R38chMbtnyWMfQgbQpzMQZgPBPUvVWivJyYEysZnQWrAYzZzRPe36VFbFy5ynWx0w=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: bbsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
+
+    it("should fail to create proof when attempting to create one with missing messages", async () => {
+      const messages = [
+        stringToBytes("badmessagex01a=="),
+        stringToBytes("PNMnARWIHP+s2g=="),
+      ];
+
+      const bbsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pbiZ/pmArLDr3oSCqthKgSZw4VFzzJMFEuHP9AAnOnUJmqkOmvI1ctGLO6kCLFuwQVAAAAA4GrOHdyZEbTWRrTwIdz+KXWcEUHdIx41XSr/RK0TE5+qU7irAhQekOGFpGWQY4rYrDxoHToB4DblaJWUgkSZQLQ5sOfJg3qUJr9MpnDNJ8nNNitL65e6mqnpfsbbT3k94LBQI3/HijeRl29y5dGcLhOxldMtx2SvQg//kWOJ/Ug8e1aVo3V07XkR1Ltx76uzA=="
+      );
+      const signature = base64Decode(
+        "qg3PfohWGvbOCZWxcWIZ779aOuNSafjCXLdDux01TTNGm/Uqhr/kZZ1wSmxKwbEWAhctrDCp2mGE0M0l6DlA5R38chMbtnyWMfQgbQpzMQZgPBPUvVWivJyYEysZnQWrAYzZzRPe36VFbFy5ynWx0w=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: bbsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
+
+    it("should fail to create proof when attempting to create one with messages supplied in wrong order", async () => {
+      const messages = [
+        stringToBytes("ti9WYhhEej85jw=="),
+        stringToBytes("PNMnARWIHP+s2g=="),
+        stringToBytes("J42AxhciOVkE9w=="),
+      ];
+
+      const bbsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pbiZ/pmArLDr3oSCqthKgSZw4VFzzJMFEuHP9AAnOnUJmqkOmvI1ctGLO6kCLFuwQVAAAAA4GrOHdyZEbTWRrTwIdz+KXWcEUHdIx41XSr/RK0TE5+qU7irAhQekOGFpGWQY4rYrDxoHToB4DblaJWUgkSZQLQ5sOfJg3qUJr9MpnDNJ8nNNitL65e6mqnpfsbbT3k94LBQI3/HijeRl29y5dGcLhOxldMtx2SvQg//kWOJ/Ug8e1aVo3V07XkR1Ltx76uzA=="
+      );
+      const signature = base64Decode(
+        "qg3PfohWGvbOCZWxcWIZ779aOuNSafjCXLdDux01TTNGm/Uqhr/kZZ1wSmxKwbEWAhctrDCp2mGE0M0l6DlA5R38chMbtnyWMfQgbQpzMQZgPBPUvVWivJyYEysZnQWrAYzZzRPe36VFbFy5ynWx0w=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: bbsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
   });
 
   describe("blsCreateProof", () => {
@@ -223,6 +331,114 @@ describe("bbsSignature", () => {
       const proof = await blsCreateProof(request);
       expect(proof).toBeInstanceOf(Uint8Array);
       expect(proof.length).toEqual(415); //TODO evaluate this length properly add a reason for this and some constants?
+    });
+
+    it("should fail to create proof when attempting to create one with an unsigned extra message", async () => {
+      const messages = [
+        stringToBytes("uiSKIfNoO2rMrA=="),
+        stringToBytes("lMoHHrFx0LxwAw=="),
+        stringToBytes("wdwqLVm9chMMnA=="),
+        stringToBytes("badmessagex01a=="),
+      ];
+
+      const blsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pb"
+      );
+      const signature = base64Decode(
+        "jU9tKKvDDqoSUTMPDGfw/1GlnOnRD56wEM2iftL7NPBlT3JxP2YY5SnR32nra0bmR//r8JH7fvgYuqpXHJB+vsYj7xoeyQtvoPZArti0YiYML2utQmsV4zN1W0sWH7+myPL/7H/m6PgxL/CjYzAaRg=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: blsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0, 1, 2, 3],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
+
+    it("should fail to create proof when attempting to create one with a modified message", async () => {
+      const messages = [
+        stringToBytes("badmessagex01a=="),
+        stringToBytes("lMoHHrFx0LxwAw=="),
+        stringToBytes("wdwqLVm9chMMnA=="),
+      ];
+
+      const blsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pb"
+      );
+      const signature = base64Decode(
+        "jU9tKKvDDqoSUTMPDGfw/1GlnOnRD56wEM2iftL7NPBlT3JxP2YY5SnR32nra0bmR//r8JH7fvgYuqpXHJB+vsYj7xoeyQtvoPZArti0YiYML2utQmsV4zN1W0sWH7+myPL/7H/m6PgxL/CjYzAaRg=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: blsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
+
+    it("should fail to create proof when attempting to create one with missing messages", async () => {
+      const messages = [
+        stringToBytes("lMoHHrFx0LxwAw=="),
+        stringToBytes("wdwqLVm9chMMnA=="),
+      ];
+
+      const blsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pb"
+      );
+      const signature = base64Decode(
+        "jU9tKKvDDqoSUTMPDGfw/1GlnOnRD56wEM2iftL7NPBlT3JxP2YY5SnR32nra0bmR//r8JH7fvgYuqpXHJB+vsYj7xoeyQtvoPZArti0YiYML2utQmsV4zN1W0sWH7+myPL/7H/m6PgxL/CjYzAaRg=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: blsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
+    });
+
+    it("should fail to create proof when attempting to create one with a modified message", async () => {
+      const messages = [
+        stringToBytes("wdwqLVm9chMMnA=="),
+        stringToBytes("lMoHHrFx0LxwAw=="),
+        stringToBytes("uiSKIfNoO2rMrA=="),
+      ];
+
+      const blsPublicKey = base64Decode(
+        "qJgttTOthlZHltz+c0PE07hx3worb/cy7QY5iwRegQ9BfwvGahdqCO9Q9xuOnF5nD/Tq6t8zm9z26EAFCiaEJnL5b50D1cHDgNxBUPEEae+4bUb3JRsHaxBdZWDOo3pb"
+      );
+      const signature = base64Decode(
+        "jU9tKKvDDqoSUTMPDGfw/1GlnOnRD56wEM2iftL7NPBlT3JxP2YY5SnR32nra0bmR//r8JH7fvgYuqpXHJB+vsYj7xoeyQtvoPZArti0YiYML2utQmsV4zN1W0sWH7+myPL/7H/m6PgxL/CjYzAaRg=="
+      );
+
+      const request: BbsCreateProofRequest = {
+        signature,
+        publicKey: blsPublicKey,
+        messages,
+        nonce: randomBytes(10),
+        revealed: [0],
+      };
+
+      await expect(createProof(request)).rejects.toThrowError(
+        "Failed to create proof"
+      );
     });
   });
 });
