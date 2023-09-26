a9b99dff8655552083e07c8bcb2b45d480aed85f
Afonso Barracha <afonso.barracha@mattr.global>
2023-09-26 17:08:16 +1300

    build(deps): remove wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,6 @@
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[macro_use]
 extern crate arrayref;
