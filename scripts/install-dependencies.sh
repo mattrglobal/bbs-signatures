53e26bb732a396814888545a5efbff3f3e53b37b
Afonso Barracha <afonso.barracha@mattr.global>
2023-09-28 10:10:10 +1300

    ci: fix rust version at 1.72.x

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,6 +2,11 @@ set -e
 
 echo ">>>> Installing Dependencies"
 
+if [ -x "$(command -v rustup)" ]; then
+    echo ">>> Installing rust version 1.72"
+    rustup install 1.72
+fi
+
 if ! [ -x "$(command -v wasm-pack)" ]; then
     echo ">>> Installing wasm-pack"
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
