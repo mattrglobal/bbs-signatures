53297cabd229f9cf932c9fd45c8c75a64c7581d0
Afonso Barracha <afonso.barracha@mattr.global>
2023-09-28 15:11:18 +1300

    refactor: always install rust 1.72

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,10 +2,8 @@ set -e
 
 echo ">>>> Installing Dependencies"
 
-if [ -x "$(command -v rustup)" ]; then
-    echo ">>> Installing rust version 1.72"
-    rustup install 1.72
-fi
+echo ">>> Installing rust version 1.72"
+rustup install 1.72
 
 if ! [ -x "$(command -v wasm-pack)" ]; then
     echo ">>> Installing wasm-pack"
