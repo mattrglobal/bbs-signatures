b929d7dadc35e6cc52d27227b990d825717c7314
Afonso Barracha <113945054+afonsobarracha@users.noreply.github.com>
2023-09-29 15:47:21 +1300

    build(deps): remove wee_alloc (#158)
    * build(deps): remove wee_alloc
    
    * build(deps): bump @wasm-tool/wasm-pack-plugin from 1.6.0 to 1.7.0
    
    * build(deps): bump wasm-bindgen dependencies
    
    * fix: remove redudant comment
    
    * ci: fix rust version at 1.72.x
    
    * build(deps): update nodejs dependencies
    
    * build(deps): bump web-sys from 0.3.39 to 0.3.64
    
    * chore: run cargo fix --edition
    
    * build(version): bump wasm version from 0.1.0 to 0.1.1
    
    * refactor: always install rust 1.72

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,6 +2,9 @@ set -e
 
 echo ">>>> Installing Dependencies"
 
+echo ">>> Installing rust version 1.72"
+rustup install 1.72
+
 if ! [ -x "$(command -v wasm-pack)" ]; then
     echo ">>> Installing wasm-pack"
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,6 @@
  * limitations under the License.
  */
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[macro_use]
 extern crate arrayref;
 
